b5999581ffe50eb4e0d334448b369e0498859ec4
Dimon <bolilui.dmytro@gmail.com>
2023-02-06 11:09:55 +0200

    Task1 - Done

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,46 @@
+import PropType from 'prop-types';
+
+import css from 'components/Profile/Profile.module.css';
+
+console.log(css);
+
+export default function Profile(userData) {
+  const { userName, tag, location, avatar, stats } = userData;
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className="name">{userName}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li className={css['stats-item']}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li className={css['stats-item']}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li className={css['stats-item']}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  userName: PropType.string,
+  tag: PropType.string,
+  location: PropType.string,
+  avatar: PropType.string,
+  stats: PropType.shape({
+    followers: PropType.number,
+    views: PropType.number,
+    likes: PropType.number,
+  }),
+};
