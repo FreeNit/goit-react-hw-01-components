6439c030c4fdd831ff46c2481169654efc2c9718
Dimon <bolilui.dmytro@gmail.com>
2023-02-07 21:52:51 +0200

    Task4 - render data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,31 @@
 import user from 'user.json';
 import data from 'data.json';
 import friends from 'friends.json';
+import transactions from 'transactions.json';
 
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         // display: 'flex',
         // justifyContent: 'center',
         // flexDirection: 'column',
         // alignItems: 'center',
         /////
         display: 'grid',
-        gridTemplateRows: '1fr 1fr',
-        gridTemplateColumns: '1fr 1fr',
+        // gridTemplateRows: '1fr 1fr',
+        gridTemplateColumns: '1fr',
         justifyContent: 'space-around',
         alignContent: 'space-around',
         alignItems: 'center',
         justifyItems: 'center',
-        gridGap: 5,
+        gridGap: 150,
         fontSize: 40,
         color: '#010101',
       }}
@@ -40,6 +42,7 @@ export const App = () => {
         <Statistics stats={data} />
       </div>
       <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,29 @@
+import PropType from 'prop-types';
+
+export default function TransactionHistory({ transactions }) {
+  console.log(transactions);
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(transaction => {
+          const { id, type, amount, currency } = transaction;
+          return (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
